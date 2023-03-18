f5438f7a0a858263b0ece4668c02bb8f0a8760b7
mcct <mcct@privat.dk>
2023-03-18 12:23:11 +0100

    Added custom user controls

---
+++ WpfBasicsWithSave/DemoMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfBasicsWithSave
+{
+    /// <summary>
+    /// Interaction logic for DemoMainWindow.xaml
+    /// </summary>
+    public partial class DemoMainWindow : Window
+    {
+        public DemoMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
