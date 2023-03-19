9a0dce9e5ea84ac2f67531b3b7394e2247f69173
mcct <mcct@privat.dk>
2023-03-19 19:32:25 +0100

    Added method for add Name to List

--- WpfBasicsWithSave/DemoMainWindow.xaml.cs
+++ WpfBasicsWithSave/DemoMainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,12 +23,48 @@ namespace WpfBasicsWithSave
     {
 
         SaveDataModel saveDataModel = new SaveDataModel(); 
+
+        // Instatiating an ObserableCollection (Similar to a List) containing FirstNameModel's where each model is a unique name ??
+        ObservableCollection<NamesModel> firstNameModels = new ObservableCollection<NamesModel>();
         public DemoMainWindow()
         {
             InitializeComponent(); // I think this kinda build the WPF window ??
 
             TxtUCSaveToFileLocation.txtLimitedInput.Text = saveDataModel.FullPath; 
-            TxtUCEnteredName.txtLimitedInput.Text = "Michael"; 
+            TxtUCEnteredName.txtLimitedInput.Text = "Michael";
+
+            // fake data
+            firstNameModels.Add(new NamesModel() { FirstName = "Bente"});
+            firstNameModels.Add(new NamesModel() { FirstName = "Knud"});
+            firstNameModels.Add(new NamesModel() { FirstName = "Hanne"});
+            firstNameModels.Add(new NamesModel() { FirstName = "Irene"});
+                       
+            //
+            LstBxNames.DisplayMemberPath = "FirstName";
+
+            // Update the ListView in the UI when the user add a new name with BtnAddName_Click
+            LstBxNames.ItemsSource = firstNameModels; // need to add a name to the listview in xaml ??
+
+
+        }
+
+        // This method run when we click the Add Name To List button
+        private void BtnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            // 
+            // firstNameModels.Add(new NamesModel() { FirstName = TxtUCEnteredName.txtLimitedInput.Text });
+
+            // Add to first index, to always show new entry on top - not in tutorial
+            firstNameModels.Add(new NamesModel() { FirstName = TxtUCEnteredName.txtLimitedInput.Text });
+
+            // Todo
+            // Clear the text field after the user press enter, to make it ready for another entry
+            TxtUCEnteredName.txtLimitedInput.Text = ""; // Not in the tutorial
+
+            // Todo
+            // Place cursor so its ready to input another name - Not in the tutorial              
+            TxtUCEnteredName.txtLimitedInput.Focus();
+
         }
     }
 }
---
+++ WpfBasicsWithSave/Models/NamesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBasicsWithSave.Models // not sure if its Model
+{
+    public class NamesModel
+    {
+        // Encapsulation
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+    }
+}
